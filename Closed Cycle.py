d22f731cc604d48a29592f362fafb67db1d289a0
Riddhi'sChaoticWorld <130882317+Riddhiman2005@users.noreply.github.com>
2023-05-24 01:01:03 +0530

    Update Closed Cycle.py

--- Closed Cycle.py
+++ Closed Cycle.py
@@ -1,1 +1,24 @@
 
+import matplotlib.pyplot as plt
+
+def closed_cycle():
+    num_points = int(input("Enter the number of points: "))
+    volumes = []
+    pressures = []
+
+    for i in range(num_points):
+        V = float(input(f"Enter the volume at point {i+1}: "))
+        P = float(input(f"Enter the pressure at point {i+1}: "))
+        volumes.append(V)
+        pressures.append(P)
+
+    volumes.append(volumes[0])
+    pressures.append(pressures[0])
+
+    plt.plot(volumes, pressures, marker='o')
+    plt.xlabel("Volume")
+    plt.ylabel("Pressure")
+    plt.title("P vs V for Closed Cycle")
+    plt.show()
+
+closed_cycle()
